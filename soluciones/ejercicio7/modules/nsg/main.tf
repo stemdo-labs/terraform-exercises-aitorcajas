bae3b876efdd9157cdf74052233c803b33522cc3
aitorcajas <acajas@stemdo.io>
2024-10-18 09:18:18 +0200

    solucion.md del ejercicio 7

--- soluciones/ejercicio7/modules/nsg/main.tf
+++ soluciones/ejercicio7/modules/nsg/main.tf
@@ -2,6 +2,22 @@ resource "azurerm_network_security_group" "nsg" {
   name                = var.name_nsg
   location            = var.location
   resource_group_name = var.resource_group_name
+  
+  security_rule {
+    name                       = "test123"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "*"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+  tags = {
+    environment = "Production"
+  }
 }
 
 resource "azurerm_subnet_network_security_group_association" "nsg-group" {
